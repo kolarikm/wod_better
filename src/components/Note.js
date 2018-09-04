b9783f536a9ab42982acf63cbfc4d6ab805b6d3b
kolarikm <kolarikm@mail.gvsu.edu>
2018-09-04 15:20:32 -0400

    Removal and status toggling

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import '../App.css';
 
 class Note extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: true
+        }
+
+        this.click = this.click.bind(this);
+    }
+
+    click() {
+        this.setState({ active: !(this.state.active) });
+    }
+
     render() {
-        return(
-            <div className="note" onClick={this.props.delete}>
-                {this.props.text}
+        let style = this.state.active ? 'enabled' : 'disabled';
+        return (
+            <div className="note">
+                <div className={style} onClick={this.click}>
+                    {this.props.text}
+                    <span className="removeBtn" onClick={this.props.delete}>x</span>
+                </div>
             </div>
         ); 
     }
